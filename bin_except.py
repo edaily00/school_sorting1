c8e1191c6a0f4d3de5a3304843b7ae558313b099
Eric Daily <dailye@oregonstate.edu>
2022-07-08 17:34:52 -0400

    yep

---
+++ bin_except.py
@@ -0,0 +1,18 @@
+class TargetNotFound(Exception):
+    pass
+
+
+def binary_search(a_list, target):
+
+    first = 0
+    last = len(a_list) - 1
+    while first <= last:
+        middle = (first + last) // 2
+        if a_list[middle] == target:
+            return middle
+        if a_list[middle] > target:
+            last = middle - 1
+        else:
+            first = middle + 1
+        raise TargetNotFound
+
